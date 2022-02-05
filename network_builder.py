5fa5ba35393ec67681f51aea5b7ab29fd32de4e4
Metro19 <40527150+Metro19@users.noreply.github.com>
2022-02-05 02:08:20 -0500

    Preliminary results of mlrose working Improvement on algorithm needed

--- network_builder.py
+++ network_builder.py
@@ -2,33 +2,45 @@
 # Calculate the fastest route through all Metro stations
 # (c) Zack Bamford
 # (Not affiliated in any way with the Washington Metropolitan Area Transportation Authority)
+
+import os
 import networkx as nx
 import wmata
 from tsp_solve import run_simulation
+import pickle
+
 # vars
 # TODO: Move to a txt or easy edit file
 TRAIN_WAIT_TIME = 6
 
 
 def create_graph():
-    # init graph
-    G = nx.DiGraph()
+    if os.path.exists("wmata_graph.p"):
+        print("Using Pickled Version")
+        return pickle.load(open("wmata_graph.p", "rb"))
 
-    # add all stations
-    for s in wmata.get_all_stations():
-        G.add_node(s.station_code, object=s)
-        print(s.station_code)
+    else:
+        # init graph
+        G = nx.DiGraph()
 
-    # add all edges
-    for e in wmata.get_all_paths_between_stations():
-        G.add_edge(e.start_code, e.end_code, weight=e.distance, object=e)
-        print(e.start_code, e.end_code)
+        # add all stations
+        for s in wmata.get_all_stations():
+            G.add_node(s.station_code, object=s)
+            print(s.station_code)
 
-    for edge in G.edges:
-        print(G[edge[0]][edge[1]])
+        # add all edges
+        for e in wmata.get_all_paths_between_stations():
+            G.add_edge(e.start_code, e.end_code, weight=e.distance, object=e)
+            print(e.start_code, e.end_code)
 
-    run_simulation(G)
+        for edge in G.edges:
+            print(G[edge[0]][edge[1]])
+
+        # pickle
+        pickle.dump(G, open("wmata_graph.p", "wb"))
+        return G
 
 
 if __name__ == "__main__":
-    create_graph()
+    G = create_graph()
+    run_simulation(G)
--- tsp_solve.py
+++ tsp_solve.py
@@ -1,23 +1,59 @@
+# WMATA TSP - wmata.py
+# Calculate the fastest route through all Metro stations
+# (c) Zack Bamford
+# (Not affiliated in any way with the Washington Metropolitan Area Transportation Authority)
+
 import mlrose
 import networkx
 
 
+def numeric_station_code(code: str, nodes: list) -> int:
+    """
+    Convert the station code into an integer for mlrose
+
+    :param nodes: A list of the Graph nodes
+    :param code: Station code
+    :return: Unique numeric code for each station code
+    """
+    full = nodes.index(code)
+    return int(full)
+
+
+def graph_to_tlist(G: networkx.Graph) -> list[tuple]:
+    """
+    Convert a graph into a list of tuples for mlrose
+
+    :param G: Graph of system
+    :return: List of tuples for mlrose
+    """
+    dists = []
+
+    # iter through edges
+    for edge in G.edges(data=True):
+        # print(edge)
+        dists.append((numeric_station_code(edge[0], list(G.nodes)), numeric_station_code(edge[1], list(G.nodes)), edge[2]["weight"]))
+
+    return dists
+
+
 def run_simulation(G: networkx.Graph):
+    """
+    Run a tsp simulation
+
+    :param G: Graph to run off of
+    :return:
+    """
+
+    # prep data
     dists = graph_to_tlist(G)
 
+    # run solutions
     f_dists = mlrose.TravellingSales(distances=dists)
     prob = mlrose.TSPOpt(length=len(G.nodes), fitness_fn=f_dists, maximize=False)
     bs, bf = mlrose.genetic_alg(prob, random_state=2)
 
+    # print data
     print(bs)
     print(bf)
 
-
-def graph_to_tlist(G: networkx.Graph) -> list[tuple]:
-    dists = []
-
-    # iter through edges
-    for edge in G.edges:
-        dists.append((edge[0], edge[1], edge[2]["weight"]))
-
-    return dists
+    # TODO: Check why fitness returns infinity
